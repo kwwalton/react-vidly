53e09d694087876129f72295f799cebb536da453
ken walton <ken.walton@sbdinc.com>
2020-12-31 13:31:15 -0500

    added joi for validation of form

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
 import Input from './common/input'
+import Joi from 'joi-browser'
 
 class LoginForm extends Component {
   state = {
     account: { username: '', password: '' },
     errors: {},
+  }
+
+  schema = {
+    username: Joi.string().required().label('Username'),
+    password: Joi.string().required().label('Password'),
   }
 
   // username = React.createRef()
@@ -14,14 +20,14 @@ class LoginForm extends Component {
   // }
 
   validate = () => {
-    const errors = {}
-    const { account } = this.state
-    if (account.username.trim() === '')
-      errors.username = 'Username is required.'
-    if (account.password.trim() === '')
-      errors.password = 'Password is required.'
+    const options = { abortEarly: false }
+    const { error } = Joi.validate(this.state.account, this.schema, options)
 
-    return Object.keys(errors).length === 0 ? null : errors
+    if (!error) return null
+
+    const errors = {}
+    error.details.map(err => (errors[err.path[0]] = err.message))
+    return errors
   }
 
   handleSubmit = e => {
@@ -35,16 +41,14 @@ class LoginForm extends Component {
   }
 
   validateProperty = ({ name, value }) => {
-    if (name === 'username') {
-      if (value.trim() === '') return 'Username is required.'
-    }
-    if (name === 'password') {
-      if (value.trim() === '') return 'Password is required.'
-    }
+    const obj = { [name]: value }
+    const schema = { [name]: this.schema[name] }
+    const { error } = Joi.validate(obj, schema)
+    return error ? error.details[0].message : null
   }
 
   handleChange = ({ currentTarget: input }) => {
-    const errors = {...this.state.errors}
+    const errors = { ...this.state.errors }
     const errorMessage = this.validateProperty(input)
     if (errorMessage) errors[input.name] = errorMessage
     else delete errors[input.name]
@@ -75,7 +79,7 @@ class LoginForm extends Component {
             onChange={this.handleChange}
             error={errors.password}
           />
-          <button className="btn btn-primary">Login</button>
+          <button className="btn btn-primary" disabled={this.validate()}>Login</button>
         </form>
       </div>
     )
